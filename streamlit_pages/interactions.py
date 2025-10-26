14307e39827d5eaf7f84930eb9c11634efadcb09
FBalzerMPE <fbalzer@mpe.mpg.de>
2025-10-26 13:53:41 +0100

    Fixed typo.

--- streamlit_pages/interactions.py
+++ streamlit_pages/interactions.py
@@ -5,7 +5,7 @@ import streamlit as st
 import random_kingdominion as rk
 
 rk.build_page_header(
-    "Domionion Rules Interactions",
+    "Dominion Rules Interactions",
     "On this page, you can explore interactions between Card-Shaped Objects (CSOs) that might catch you off-guard rules-wise. The project was initiated on discord by `nave`; while there are lot of different combinations, these do not cover the straight-forward interactions like your typical Village/Smithy. The interactions have been gathered with the assumption that those using it have a base knowledge of the rules of Dominion and are looking for edge-cases and things that are not 100 % intuitive.\\\nYou can either filter the interactions for certain expansions, or post a comma-separated list from which the site will try to filter for any relevant interactions.\\\nIf you want to see the full list of CSOs, you can visit the [CSO Database](/cso_overview).",
     "Learn more about the CSO and kingdom qualities on the about page.",
 )
